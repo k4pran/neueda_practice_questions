e5c85644ba145eaaa8c4019b48a0f16c913916dc
Ryan McCauley (MCC12068703) <MCC12068703@belfastmet.ac.uk>
2018-12-07 10:37:38 +0000

    - Add validation to user input

--- PRACTICE-02-CountUpTo/src/CountUpTo.java
+++ PRACTICE-02-CountUpTo/src/CountUpTo.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -10,8 +11,25 @@ public class CountUpTo {
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please enter a number to count up to");
-		int countTarget = scanner.nextInt();
+		int countTarget = 1;
+		
+		boolean validInput = false;
+		while (!validInput) {
+			System.out.println("Please enter a number to count up to. Must be 1 or greater");
+			try {
+				countTarget = scanner.nextInt();
+				if (countTarget < 1) {
+					System.out.println("Invalid input. Number must be 1 or greater.");
+				}
+				else {
+					validInput = true;
+				}
+			}
+			catch (InputMismatchException e) {
+				System.out.println("Invalid input. Input must be a number.");
+				scanner.next();
+			}
+		}
 		
 		System.out.println("--Results--\n");
 		for (int i = 1; i <= countTarget; i++) {
